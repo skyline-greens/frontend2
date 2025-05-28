1dca2fa68ae50a63dfa98f05c523fcb75830a1d2
Fendi Mohamed <m.fendi@esi-sba.dz>
2025-05-28 03:26:45 +0100

    update dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,80 @@
+import { getWarehouses, getWarehousesStats, Warehouse } from '@/actions/warehouse';
+import PageContainer from '@/components/layout/page-container';
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { DataTableSkeleton } from '@/components/ui/table/data-table-skeleton';
+import ProductListingPage from '@/features/warehouses/components/product-listing';
+import { searchParamsCache } from '@/lib/searchparams';
+import { cn } from '@/lib/utils';
+import { SearchParams } from 'nuqs/server';
+import { Suspense } from 'react';
+import NewWarehouseDialog from '@/features/warehouses/components/new-warehouse-dialog';
+import { WarehouseStatsCards } from '@/features/warehouses/components/stats/warehouse-stats';
+export const metadata = {
+  title: 'Dashboard: Warehouses',
+};
 
-import { redirect } from 'next/navigation';
-import { getAuth } from '@/actions/auth';
-export default async function Dashboard() {
-  // const { userId } = await auth();
-    const auth = await getAuth();
+type pageProps = {
+  searchParams: Promise<SearchParams>;
+};
 
+export default async function Page(props: pageProps) {
+  let warehousesStats: any = {};
+      try {
+        warehousesStats = await getWarehousesStats();
+      } catch (err) {
+        console.error("Failed to fetch warehouses data:", err);
+       
+      }
+  
+      console.log('Page data:', warehousesStats);
 
-  if (!auth.isAuth) {
-    return redirect('/auth/sign-in');
-  } else {
-    redirect('/dashboard/overview');
+  const searchParams = await props.searchParams;
+  // Allow nested RSCs to access the search params (in a type-safe way)
+  searchParamsCache.parse(searchParams);
+  let warehouses: Warehouse[] = [];
+  try {
+    warehouses = await getWarehouses();
+  } catch (err) {
+    console.error("Failed to fetch warehouses data:", err);
+   
   }
+  // This key is used for invoke suspense if any of the search params changed (used for filters).
+  // const key = serialize({ ...searchParams });
+
+console.log('Page data:', warehouses);
+
+  return (
+    <PageContainer scrollable={false}>
+
+      <div className='flex flex-1 flex-col space-y-4'>
+        <div >
+        <WarehouseStatsCards
+      stats={warehousesStats}/>
+        </div>
+        <div className='flex items-start justify-between'>
+          <Heading
+            title='Warehouses'
+            description='Manage Warehouses in your agriculture system'
+          />
+            <NewWarehouseDialog />
+          {/* <Link
+            href='/dashboard/product/new'
+            className={cn(buttonVariants(), 'bg-green-700 text-xs md:text-sm')}
+          >
+            <IconPlus className='mr-2 h-4 w-4' /> Add New Warehouse
+          </Link> */}
+        </div>
+        <Separator />
+        <Suspense
+          // key={key}S
+          fallback={
+            <DataTableSkeleton columnCount={5} rowCount={8} filterCount={2} />
+          }
+        >
+          <ProductListingPage data={warehouses}  />
+        </Suspense>
+      </div>
+    </PageContainer>
+  );
 }
--- src/constants/mqtt.ts
+++ src/constants/mqtt.ts
@@ -1,4 +1,1 @@
-export const CELL_ID =
-  typeof window !== "undefined"
-    ? localStorage.getItem("CELL_ID")
-    : process.env.CELL_ID || null;
+export const CELL_ID = process.env.CELL_ID || "beb56e75-b158-4e33-9125-84f0c3b3d618";
